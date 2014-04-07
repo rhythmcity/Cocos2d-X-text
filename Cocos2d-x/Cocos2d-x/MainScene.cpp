a54ccc340e8993330f2f6506e1ef18b4865c73a9
liyan <707704996@qq.com>
2014-04-07 18:08:11 +0800

    贝塞尔曲线

--- Cocos2d-x/Cocos2d-x/MainScene.cpp
+++ Cocos2d-x/Cocos2d-x/MainScene.cpp
@@ -28,7 +28,7 @@ bool MainScene::init(){
     this->addChild(menu);
     sprite=CCSprite::create("Icon-72.png");
    CCSize winsize= CCDirector::sharedDirector()->getWinSize();
-    sprite->setPosition(ccp(winsize.width/2, winsize.height/2));
+    sprite->setPosition(ccp(300, 300));
     this->addChild(sprite);
     
 
@@ -36,26 +36,15 @@ bool MainScene::init(){
     return true;
 }
 void MainScene:: onMenuItem(CCObject *object){
-     /*动画*/
-//    CCHide *hide= CCHide::create();
-//    CCFadeTo *to=CCFadeTo::create(2, 100);//  0~255
-//    CCFadeOut *out=CCFadeOut ::create(3);
-//    CCMoveTo *moveto=CCMoveTo::create(3, ccp(800, sprite->getPositionY()));//移动到什么位置 X ， Y
-//    CCMoveBy *moveby=CCMoveBy::create(3, ccp(300, 0));//移动多少位置 X，Y
-  //  CCJumpTo *junmto=CCJumpTo::create(2, ccp(sprite->getPositionX()+300, sprite->getPositionY()), 100, 1);//跳到哪
-//    CCJumpBy *junmby=CCJumpBy::create(2, ccp(300, 0), 100, 1);//跳多少
-    CCPointArray *pointArr=CCPointArray::create(5);
-    pointArr->addControlPoint(ccp(0,0));
-    pointArr->addControlPoint(ccp(100, 0));
-    pointArr->addControlPoint(ccp(100, -50));
-    pointArr->addControlPoint(ccp(150, -50));
-    pointArr->addControlPoint(ccp(150, 0));
-    pointArr->addControlPoint(ccp(250, 0));
     
-    CCCardinalSplineBy *by=CCCardinalSplineBy ::create(3, pointArr, 1);//按轨迹移动  tension 张力   1 为 直线运动
+    ccBezierConfig  bc;
+    bc.controlPoint_1=ccp(400, 400);
+    bc.controlPoint_2=ccp(800, 200);
+    bc.endPosition=ccp(900, 300);
+    CCBezierTo *to=CCBezierTo ::create(3, bc);//贝塞尔曲线
     
     
-    sprite->runAction(by);
+    sprite->runAction(to);
     
 
 }
